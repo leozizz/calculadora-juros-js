9fb8e659b31c1d99775c0ce8d86bc601da0f3ae0
leozizz <leonardocps2015@gmail.com>
2022-10-07 15:51:37 -0300

    Inserindo processamento de dados

--- juros.js
+++ juros.js
@@ -5,16 +5,29 @@ let divida = {
     diasAtraso: ''
 }
 
+let juros = ''
+
 console.log("Aplicação de Juros:\n")
 
-divida.valorDivida = entradaDados.question("Insira o valor da divida: ")
-divida.diasAtraso = entradaDados.question("Inisira os dias de atraso:")
+divida.valorDivida = entradaDados.question("Informe o valor devido: R$ ")
+divida.diasAtraso = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ")
 
 if (divida.valorDivida == 0) {
     console.log("\nVocê não tem dividas em aberto")
-} else if (divida.diasAtraso == 0) {
+} 
+else if (divida.diasAtraso == 0) 
+{
     console.log("\nVocê não tem dividas em atraso")
-} else {
+} 
+else if (divida.valorDivida >= 15) 
+{
+    let valorJuros = (Number(divida.valorDivida) / 100) * juros
+    let totalDivida = Number(divida.valorDivida) + valorJuros
+    console.log(valorJuros)
+    console.log(totalDivida)
+} 
+else 
+{
     console.log(`\nValor original da dívida: R$ ${divida.valorDivida}`)
     console.log(`Dias atrasados: ${divida.diasAtraso} dias`)
     console.log(`Taxa de Juros: 0%`)
