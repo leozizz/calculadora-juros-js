633a5d71cb28691eb5d004e72e4504b533a82f9c
leozizz <leonardocps2015@gmail.com>
2022-10-10 16:04:08 -0300

    Processamento de Dados - Inserindo Calculo

--- juros.js
+++ juros.js
@@ -19,17 +19,23 @@ else if (divida.diasAtraso == 0)
 {
     console.log("\nVocê não tem dividas em atraso")
 } 
-else if (divida.valorDivida >= 15) 
+else if (divida.diasAtraso <= 15) 
 {
+    juros = 5
     let valorJuros = (Number(divida.valorDivida) / 100) * juros
     let totalDivida = Number(divida.valorDivida) + valorJuros
-    console.log(valorJuros)
-    console.log(totalDivida)
+    console.log(`\nValor original da dívida: R$ ${divida.valorDivida}`)
+    console.log(`Dias atrasados: ${divida.diasAtraso} dias`)
+    console.log(`Taxa de Juros: ${juros}%`)
+    console.log(`Valor total com juros: R$ ${totalDivida}`)
 } 
 else 
 {
+    let juros = 10
+    let valorJuros = (Number(divida.valorDivida) / 100) * juros
+    let totalDivida = Number(divida.valorDivida) + valorJuros
     console.log(`\nValor original da dívida: R$ ${divida.valorDivida}`)
     console.log(`Dias atrasados: ${divida.diasAtraso} dias`)
-    console.log(`Taxa de Juros: 0%`)
-    console.log(`Valor total com juros: R$0`)
+    console.log(`Taxa de Juros: ${juros}%`)
+    console.log(`Valor total com juros: R$ ${totalDivida}`)
 }
